ab21ec41cef472fd2bd3e7c4751bbfbfb54271d1
Hampampster <siegfried.bartram@icloud.com>
2025-03-18 12:00:36 +0000

    Snakes-and-ladders.py

--- Snakes-and-ladders.py
+++ Snakes-and-ladders.py
@@ -1,1 +1,15 @@
 import time
+import os
+
+y0 = [" 1"," 2"," 3"," 4"," 5"," 6"," 7"," 8"," 9","10"]
+y1 = ["11","12","13","14","15","16","17","18","19","20"]
+ylist = [y0, y1]
+
+def printTable():
+  for i in ylist:
+    printY = ""
+    for y in i:
+      printY += y
+    print(printY)
+
+os.system('clear')
