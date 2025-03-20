66cdcfcf9ecf16b7f449428e1c54570c3f8698cb
Hampampster <siegfried.bartram@icloud.com>
2025-03-20 13:47:35 +0000

    Update Snakes-and-ladders.py

--- Snakes-and-ladders.py
+++ Snakes-and-ladders.py
@@ -3,17 +3,18 @@ import random
 import os
 
 AmPlayers = 0
-while AmPlayers < 1 or AmPlayers == str():
-  AmPlayers = int(input("How many people are playing the game?"))
+while AmPlayers < 1:
+  AmPlayers = input("How many people are playing the game?")
+  AmPlayers = int(AmPlayers)
 players = []
-playerPos = ["","","","","","","",""]
+playerPos = [0,0,0,0,0,0,0]
 i = 0
 for x in range(1,int(AmPlayers)):
   i += 1
   players.extend(str(i))
   
 input("press enter to start")
-y0 = [" 1"," 2"," 3"," 4"," 5"," 6"," 7"," 8"," 9","10"]
+y0 = ["01","02","03","04","05","06","07","08","09","10"]
 y1 = ["11","12","13","14","15","16","17","18","19","20"]
 y2 = ["21","22","23","24","25","26","27","28","29","30"]
 y3 = ["31","32","33","34","35","36","37","38","39","40"]
@@ -26,18 +27,20 @@ y9 = ["91","92","93","94","95","96","97","98","99","100"]
 
 ylist = [y0, y1, y2, y3, y4, y5, y6, y7, y8, y9]
 
-def checkForPlayerPOS(length, y):
-  for i in range(1,length):
+def checkForPlayerPOS(y):
+  for i in range(0,AmPlayers):
     if playerPos[i] == y:
-      return "Pl"
-    else:
-      return str(y)
+      return ("P" + str(i+1))
+  return str(y)
 
 def printTable():
   for i in ylist:
     printY = ""
     for y in i:
-      printY += checkForPlayerPOS(len(players), int(y))
+      ConcCurr = str(checkForPlayerPOS(int(y)))
+      printY += ConcCurr
+      if i == y0 and not 'P' in ConcCurr:
+        printY += " "
       printY += "  "
     print(printY)
 
@@ -59,8 +62,6 @@ def gen_snakes():
     snakes.write(str(snakestart)+","+str(snakeend)+"\n")
   snakes.close()
   return(numsnakes)
-  
-  
 
 def checksnakes(playloc, numsnakes):
   snakes = open("snakes.txt", "r")
@@ -79,11 +80,12 @@ def RTD():
   return diceRoll
 
 def playerMove():
-  for i in range(1,AmPlayers):
-    playerPos[i] = RTD()  
+  for i in range(0,AmPlayers):
+    playerPos[i] += RTD()  
 
 while True:
-  
+  printTable()
+  print(playerPos)
+  input("Move players")
   playerMove()
   os.system('clear')
-  printTable()
