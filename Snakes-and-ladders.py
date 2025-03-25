dc9ec2091705e481d861a0b8c511bc70460b461b
Hampampster <siegfried.bartram@icloud.com>
2025-03-25 09:36:42 +0000

    Update Snakes-and-ladders.py

--- Snakes-and-ladders.py
+++ Snakes-and-ladders.py
@@ -4,19 +4,19 @@ import os
 
 AmPlayers = 0
 while AmPlayers < 1:
-  AmPlayers = input("How many people are playing the game?")
+  AmPlayers = input("How many people are playing the game?") #asks how many people are playing the game
   AmPlayers = int(AmPlayers)
-players = []
-playerPos = [0,0,0,0,0,0,0]
+players = [] #stores the players
+playerPos = [0,0,0,0,0,0,0] #stores the players position
 i = 0
 for x in range(1,int(AmPlayers)):
   i += 1
-  players.extend(str(i))
+  players.extend(str(i)) #idk what this does it doesnt really work
 
 
 
 input("press enter to start")
-y0 = ["01","02","03","04","05","06","07","08","09","10"]
+y0 = ["01","02","03","04","05","06","07","08","09","10"] #big table of the board
 y1 = ["20","19","18","17","16","15","14","13","12","11"]
 y2 = ["21","22","23","24","25","26","27","28","29","30"]
 y3 = ["40","39","38","37","36","35","34","33","32","31"]
@@ -28,33 +28,33 @@ y8 = ["81","82","83","84","85","86","87","88","89","90"]
 y9 = ["100","99","98","97","96","95","94","93","92","91"]
 
 
-ylist = [y9,y8,y7,y6,y5,y4,y3,y2,y1,y0]
+ylist = [y9,y8,y7,y6,y5,y4,y3,y2,y1,y0] #construct the board
 
-def checkForPlayerPOS(y):
+def checkForPlayerPOS(y): #must i explain this?
   for i in range(0,AmPlayers):
     if playerPos[i] == y:
       return ("P" + str(i+1))
   return str(y)
 
-def printTable(interpolate, player):
+def printTable(interpolate, player): #prints the table and the players' positions
   for i in ylist:
     if i == y9:
-      printY = ""
+      printY = "" #reset the line
     else:
       printY = " "
     for y in i:
       ConcCurr = str(checkForPlayerPOS(int(y)))
-      if i == y0 and not y == "10" and not 'P' in ConcCurr:
-        printY += " "
-      if interpolate == 1 and ('P' + str(player)) in ConcCurr:
-        printY += y
-        printY += 'P' + str(player+1)
+      if i == y0 and not y == "10" and not 'P' in ConcCurr: #checks if the y = the bottom layer
+        printY += " " #adds an extra space
+      if interpolate == 1 and ('P' + str(player)) in ConcCurr: #icr what interpolate
+        printY += y 
+        printY += 'P' + str(player+1) #prints the player at the player position
       else:
         printY += ConcCurr
-        printY += "| "
-    print(printY)
+        printY += "| " #cool litle border to show the segments on the board
+    print(printY) #print one line
 
-def printList(DaList):
+def printList(DaList): #prints the players position for easy reading
   DaPrint = ""
   for i in range(AmPlayers):
     DaPrint += "P"
@@ -64,23 +64,23 @@ def printList(DaList):
     DaPrint += "  "
   return DaPrint
 
-def gen_lads():
+def gen_lads(): #i dont need to explain thtis
   numlads = random.randint(7,15)
   ladbottoms = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
   ladtops = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
   for i in range(numlads):
-    ladbottoms[i] = random.randint(1,89)
-    ladtops[i] = random.randint(ladbottoms[i]+1, 100)
-  return numlads, ladbottoms, ladtops
+    ladbottoms[i] = random.randint(1,89) #generate the bottom of a ladder on any layer from 0-8
+    ladtops[i] = random.randint(ladbottoms[i]+1, 100) #generate the top from bottom to 100
+  return numlads, ladbottoms, ladtops #return all the data to be set as variables
 
-def gen_snakes():
+def gen_snakes(): #i dont need to explain this either
   numsnakes = random.randint(7,15)
   snakeHeads = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
   snakeTails = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
   for i in range(numsnakes):
-    snakeHeads[i] = random.randint(11,99)
-    snakeTails[i] = random.randint(1,snakeHeads[i])
-  return numsnakes, snakeHeads, snakeTails
+    snakeHeads[i] = random.randint(11,99) # generate the head of a snake from layer 1-9
+    snakeTails[i] = random.randint(1,snakeHeads[i]) # generate the tail of the snake from layer0-the snake head
+  return numsnakes, snakeHeads, snakeTails #same as the ladders
 
 def checksnakes(player):
   for i in range(numsnakes):
